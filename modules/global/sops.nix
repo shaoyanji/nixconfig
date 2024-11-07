2f2140102aae80f52ca55e086f828f593cd6322d
shaoyanji <100967396+shaoyanji@users.noreply.github.com>
2024-11-07 12:43:36 +0100

    chore: update

--- modules/global/sops.nix
+++ modules/global/sops.nix
@@ -2,8 +2,6 @@
 let
   local_ssh_key= "local/mb1/ssh/private-key";
   #local_ssh_key= "local/ps1xp/ssh/private-key";
-  #local_ssh_key= "local/mb1/ssh/private-key";
-  #local_ssh_key= "local/ps1xp/ssh/private-key";
   #local_ssh_key= "local/bizmac/ssh/private-key";
   ssh_key_path = "${config.home.homeDirectory}/.ssh/id_ed25519";
   age_key_path = "${config.home.homeDirectory}/.config/sops/age/keys.txt";

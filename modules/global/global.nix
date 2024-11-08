ebb0ece2e7b543b2a361ee52da3ee3000539c7e8
shaoyanji <100967396+shaoyanji@users.noreply.github.com>
2024-11-08 11:59:08 +0100

    nix-build: config update

--- modules/global/global.nix
+++ modules/global/global.nix
@@ -6,13 +6,27 @@
 
  # Nix configuration ------------------------------------------------------------------------------
   #
+  nix = {
+    gc = {
+      automatic = true;
+      interval = {
+        Hour = 3;
+        Minute = 30;
+      };
+      options = "--delete-older-than 10d";
+      package = inputs.nix.packages.${pkgs.hostPlatform.system}.nix;
+    };
+
+
   nix.settings = {
     substituters= [
       "https://cache.nixos.org/"
+      "https://nix-community.cachix.org"
       "https://hyprland.cachix.org"
     ];
     trusted-public-keys= [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
     ];
       trusted-users = [

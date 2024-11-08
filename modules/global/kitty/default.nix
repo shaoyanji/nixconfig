71fbaeedc0be73b40455d0f7f89954a11a06d753
shaoyanji <100967396+shaoyanji@users.noreply.github.com>
2024-11-08 18:10:48 +0100

    kitty configuration fixed with nix, usr/bin/kitty for chromebook makes it less nix-y

--- modules/global/kitty/default.nix
+++ modules/global/kitty/default.nix
@@ -1,24 +1,23 @@
 { config, pkgs, ... }:
 
 {
-  programs.kitty = {
-    enable = true;
-    settings = {
-      startup_session = "Default";
-      font_family = "JetBrainsMono NF";
-      font_size = 12;
-      scrollback_lines = 10000;
-      tab_bar_margin_width = 1;
-      tab_bar_style = "powerline";
-      tab_bar_min_tabs = 2;
-      tab_bar_max_tabs = 10;
-      mouse_map = "default";
-      cursor_shape = "block";
-      cursor_blink_interval = 0.5;
-      cursor_stop_blinking_after = 0.5;
-      enable_audio_bell = false;
-      include = "~/.config/kitty/theme.conf";
-    };
-  };
-  home.file.".config/kitty/theme.conf".source = ./current-theme.conf;
+  #  programs.kitty = {
+  #    enable = true;
+  #    settings = {
+  #      font_family = "JetBrainsMono";
+  #      font_size = 12;
+  #      background_opacity = 0.9;
+  #      scrollback_lines = 10000;
+  #      tab_bar_margin_width = 1;
+  #      tab_bar_style = "powerline";
+  #      tab_bar_min_tabs = 2;
+  #      cursor_shape = "block";
+  #      cursor_blink_interval = 0.5;
+  #      cursor_stop_blinking_after = 0.5;
+  #      enable_audio_bell = false;
+  #      include = "~/.config/kitty/theme.conf";
+  #    };
+  #  };
+  home.file.".config/kitty/kitty.conf".source = ./kitty.conf;
+  home.file.".config/kitty/current-theme.conf".source = ./current-theme.conf;
 }

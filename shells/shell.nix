0b131f4f37820864d0f5481afa9daedb0c25db9f
shaoyanji <jisifu@gmail.com>
2024-11-09 19:37:36 +0000

    stateof the pi

--- shells/shell.nix
+++ shells/shell.nix
@@ -6,60 +6,43 @@
   pkgs.mkShell
 # pkgs.mkShellNoCC 
 {
-  
   packages = with pkgs; [
     cowsay
     lolcat
-    #    oh-my-posh 
-    fzf
-    zoxide
-    bat
-    eza
-    ripgrep
-    #    bitwarden-cli # doesn't work on macOS
-    spotube
-  # base tools 
-	# dev tools
-	    #	duckdb
-	    #	xcbuild
-	    #	coreutils
-      #	wkhtmltopdf
-	# languages
-	    #	python3
-	    #	nodejs
-	    #	nodePackages.node2nix
-	    #	go
-  # shell tools
-    # minimalistic jobby tools
-    # helix
-    fastfetch
-    hyperfine
-    #lf
-  # aitools
-    #ollama
+# aitools
     #aichat
     tgpt
-    #sops
     #mods
-  # pdf workflow
-    # pandoc
-    # texlive.combined.scheme-small
-    # tgpt
-    # pdfcpu
-    # poppler_utils
-    # wkhtmltopdf
+# ui
+    gum
+# utilities
+    zoxide
+    fzf
+    bat
+    ripgrep
+    eza
+    lf
+    starship
+    direnv
+    go-task
+    yq-go
+    nushell
+    git
+# secrets management
+    sops
+    bitwarden-cli
+# editor
+    neovim
   ];
   GREETING = "Hello, Nix!";
-   shellHook = /*sh*/ ''
-   #zsh
-   #   export EDITOR='hx'
-   #  
+   shellHook = ''
    eval "$(zoxide init bash)"
    eval "$(fzf --bash)"
    0file() { curl -F"file=@$1" https://envs.sh ; }
    0pb() { curl -F"file=@-;" https://envs.sh ; }
    0url() { curl -F"url=$1" https://envs.sh ; }
    0short() { curl -F"shorten=$1" https://envs.sh ; }
+   BW_SESSION="oB8LrP9CMoqGmlPPe89YhS8fghHQh+G/qmx1if2Qnr+aS+GuJRhTkFz+UFMc86ccPZ2L9nFJjP5FWF86XkeAGg=="
    alias l='eza -lahF --color=auto --icons --sort=size --group-directories-first'
    alias lss='eza -hF --color=auto --icons --sort=size --group-directories-first'
    alias la='eza -ahF --color=auto --icons --sort=size --group-directories-first'
@@ -68,6 +51,11 @@
    alias lt='eza -aT --icons --group-directories-first --color=auto --sort=size'
    alias cat='bat'
    alias grep='rg'
+ 
+   EDITOR=nvim
    echo $GREETING | cowsay | lolcat
-   '';
+   eval "$(task --completion bash)"
+   eval "$(direnv hook bash)"
+   eval "$(starship init bash)"
+  '';
 }

30276f8189663cdc75ddba83c5c0ad24faaecac2
shaoyanji <100967396+shaoyanji@users.noreply.github.com>
2024-11-09 12:03:07 +0100

    nix-build: config update

--- modules/global/global.nix
+++ modules/global/global.nix
@@ -22,6 +22,7 @@
     trusted-public-keys= [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "shaoyanji.cachix.org-1:3XUZGFcaq5bXFKwtCR+POG81Hh6WfTqf50Bmz4VHpj0="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
     ];
       trusted-users = [
@@ -40,6 +41,7 @@
   ];
   environment.systemPackages = with pkgs; [
     nixd
+    devenv
   ];
   # Create /etc/bashrc that loads the nix-darwin environment.
   #programs.zsh.enable = true;

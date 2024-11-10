1484997b57464c6bb097176577c316facf622972
shaoyanji <jisifu@gmail.com>
2024-11-10 18:46:24 +0100

    wsl is guckloch

---
+++ hosts/guckloch/configuration.nix
@@ -0,0 +1,26 @@
+{ nixos-wsl, home-manager, config, lib, pkgs, ... }:
+
+{
+  imports = [
+
+  ];
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  wsl.enable = true;
+  wsl.defaultUser = "devji";
+  wsl.docker-desktop.enable = true;
+  wsl.useWindowDriver = true;
+  virtualisation.docker.enable = true;
+  users.users.devji.extraGroups = [ "docker" ];
+
+  environment.systemPackages = with pkgs; [
+  ];
+
+  programs.nix-ld = {
+    enable = true;
+    package = pkgs.nix-ld-rs;
+  };
+  
+  system.stateVersion = "24.05";
+
+}
+

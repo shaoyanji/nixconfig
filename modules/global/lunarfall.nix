b7848ec2a166d0cac2c7abe3bf7fb7f9cc7ec9d4
shaoyanji <100967396+shaoyanji@users.noreply.github.com>
2024-11-12 15:39:09 +0100

    nix-build: config update

--- modules/global/lunarfall.nix
+++ modules/global/lunarfall.nix
@@ -1,23 +1,8 @@
 { pkgs, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
-  home.username = "devji";
-  home.homeDirectory= "/home/devji";
-
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "24.11"; # Please read the comment before changing.
-
   imports = [ 
-    ./home.nix
-    ./browser/firefox.nix
+    ./heim.nix
   ];
   
   nixpkgs.config.allowUnfree = true;
@@ -29,7 +14,6 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
-    obsidian
   ];
  # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
@@ -41,30 +25,13 @@
    # # You can also set the file content immediately.
     # ".gradle/gradle.properties".text = ''
     #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
+    #   org.gradle.daemon.idletimeout=3600
     # '';
   };
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-    home.sessionVariables = {
+   home.sessionVariables = {
       XDG_DATA_DIRS="$XDG_DATA_DIRS:$HOME/.nix-profile/share:/nix/var/nix/profiles/default/share";
       PATH="$HOME/.nix-profile/bin:/nix/var/nix/profiles/default/bin:$PATH";
 
    };
-
-  # Let Home Manager install and manage itself.
-    programs.home-manager.enable = true;
 }

f1a2516d87bea6816382dd215a4a4103e5ad692e
shaoyanji <100967396+shaoyanji@users.noreply.github.com>
2024-11-12 19:20:24 +0100

    nix-build: config update

--- modules/global/lunarfall.nix
+++ modules/global/lunarfall.nix
@@ -14,6 +14,7 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
+    devenv
   ];
  # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.

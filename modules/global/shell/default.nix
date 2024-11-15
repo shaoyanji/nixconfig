5e8c7c815a6b829f09240efb728e5f763201684e
shaoyanji <matt@bountystash.com>
2024-11-15 12:10:56 +0100

    nix-build: config update

--- modules/global/shell/default.nix
+++ modules/global/shell/default.nix
@@ -112,6 +112,39 @@ in
     };
     fzf.enable = true;
   };
+  programs.tmux = {
+    enable = true;
+    terminal = "xterm-256color";
+    shell = "${pkgs.zsh}/bin/zsh";
+      #shortcut = "a";
+    historyLimit = 100000;
+    baseIndex = 1;
+    plugins = with pkgs.tmuxPlugins; [ 
+        yank
+        resurrect
+        continuum
+        tmux-fzf
+        vim-tmux-navigator
+        fzf-tmux-url
+        catppuccin
+        better-mouse-mode
+    ];
+    extraConfig = /*tmux*/ ''
+      set -g mouse on
+      bind h select-pane -L
+      bind j select-pane -D
+      bind k select-pane -U
+      bind l select-pane -R
+      bind | split-window -h -c "#{pane_current_path}"
+      bind - split-window -v -c "#{pane_current_path}"
+      bind c new-window -c "#{pane_current_path}"
+      set -g @catppuccin_flavours "mocha"
+      set -g @continuum-restore "on"
+      set -g @continuum-boot "on"
+      set -g @resurrect-strategy-nvim "session"
+      set -g @resurrect-capture-pane-contents "on"
+    '';
+  };
   home.packages = with pkgs;
     [
       thefuck
@@ -127,7 +160,7 @@ in
 
   home.file = {
     #    ".zshrc".source = ./.zshrc;
-    ".tmux.conf".source = ./.tmux.conf;
+    # ".tmux.conf".source = ./.tmux.conf;
 
     # # You can also set the file content immediately.
 

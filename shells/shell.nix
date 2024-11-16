d1699a9ddde8ba4f379f7537ada4832d42db0b81
shaoyanji <matt@bountystash.com>
2024-11-16 09:24:34 +0100

    nix-build: config update

--- shells/shell.nix
+++ shells/shell.nix
@@ -10,13 +10,13 @@
     cowsay
     lolcat
 # aitools
-    #spotube
     #aichat
     tgpt
     #mods
 # ui
     gum
     pop
+    charm-freeze
 # utilities
     zoxide
     fzf
@@ -29,25 +29,51 @@
     go-task
     yq-go
     nushell
+# external
     git
-    # charm-freeze
+# workflow/languages
+    # python
+    # rust
+    # go
+    # luajit
     # pandoc
+# misc
+    # texlive-scheme-small
+    # ghostscript
+    fastfetch
+    speedtest-cli
+    btop
+    htop
+# entertainment
+    # yt-dlp
+    # cmus
+    # spotube
 # secrets management
     sops
     bitwarden-cli
+    # gpg
+    # age
+    # pass
 # editor
     # neovim
-
+    # helix
+    # kakoune
+    # vis
+    # emacs
+    # vscode
   ];
+
   GREETING = "Hello, Nix!";
    shellHook = ''
    eval "$(zoxide init bash)"
    eval "$(fzf --bash)"
+   eval "$(task --completion bash)"
+   eval "$(direnv hook bash)"
+   eval "$(starship init bash)"
    0file() { curl -F"file=@$1" https://envs.sh ; }
    0pb() { curl -F"file=@-;" https://envs.sh ; }
    0url() { curl -F"url=$1" https://envs.sh ; }
    0short() { curl -F"shorten=$1" https://envs.sh ; }
-   BW_SESSION="oB8LrP9CMoqGmlPPe89YhS8fghHQh+G/qmx1if2Qnr+aS+GuJRhTkFz+UFMc86ccPZ2L9nFJjP5FWF86XkeAGg=="
    alias l='eza -lahF --color=auto --icons --sort=size --group-directories-first'
    alias lss='eza -hF --color=auto --icons --sort=size --group-directories-first'
    alias la='eza -ahF --color=auto --icons --sort=size --group-directories-first'
@@ -56,13 +82,13 @@
    alias lt='eza -aT --icons --group-directories-first --color=auto --sort=size'
    alias cat='bat'
    alias grep='rg'
- 
-   EDITOR=nvim
+
    echo $GREETING | cowsay | lolcat
-   eval "$(task --completion bash)"
-   eval "$(direnv hook bash)"
-   eval "$(starship init bash)"
+
+   # secrets management
+   BW_SESSION="oB8LrP9CMoqGmlPPe89YhS8fghHQh+G/qmx1if2Qnr+aS+GuJRhTkFz+UFMc86ccPZ2L9nFJjP5FWF86XkeAGg=="
    curl -s https://raw.githubusercontent.com/shaoyanji/nixconfig/refs/heads/main/modules/global/secrets/secrets.yaml -o .temp.yaml
+
    # pop settings
    export RESEND_API_KEY=$(sops -d --extract '["RESEND"]["API"]["KEY"]' .temp.yaml)
    # export POP_SMTP_HOST=smtp.gmail.com

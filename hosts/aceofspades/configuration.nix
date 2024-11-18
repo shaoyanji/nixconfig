56d651a388e14eb51dd2646bd9f0bb73b597110c
shaoyanji <100967396+shaoyanji@users.noreply.github.com>
2024-11-18 10:19:57 +0100

    chore: update

--- hosts/aceofspades/configuration.nix
+++ hosts/aceofspades/configuration.nix
@@ -26,7 +26,6 @@
   networking.hostName = "aceofspades"; # Define your hostname.
 	#   networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   services.xserver.digimend.enable = true;
-  inputs.zen-browser.packages.${pkgs.system}.generic
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
@@ -144,6 +143,7 @@
   mpv
   config.boot.kernelPackages.digimend
 #  nfs-utils
+  inputs.zen-browser.packages.${pkgs.system}.generic
   ];
 
   # Some programs need SUID wrappers, can be configured further or are

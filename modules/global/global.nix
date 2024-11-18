8f93f6e52d0c72cc44d889ce39d82304d492cc46
shaoyanji <matt@bountystash.com>
2024-11-18 16:22:51 +0100

    nix-build: config update

--- modules/global/global.nix
+++ modules/global/global.nix
@@ -17,11 +17,14 @@
     substituters= [
       "https://cache.nixos.org/"
       "https://nix-community.cachix.org"
+      "https://chaotic-nyx.cachix.org/"
+      "https://shaoyanji.cachix.org"
       "https://hyprland.cachix.org"
     ];
     trusted-public-keys= [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
       "shaoyanji.cachix.org-1:3XUZGFcaq5bXFKwtCR+POG81Hh6WfTqf50Bmz4VHpj0="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
     ];

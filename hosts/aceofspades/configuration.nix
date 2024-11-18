52da6a920e2493c7efd4a9483c2f4de4b4ed06a9
shaoyanji <100967396+shaoyanji@users.noreply.github.com>
2024-11-18 10:16:56 +0100

    chore: update

--- hosts/aceofspades/configuration.nix
+++ hosts/aceofspades/configuration.nix
@@ -26,7 +26,7 @@
   networking.hostName = "aceofspades"; # Define your hostname.
 	#   networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   services.xserver.digimend.enable = true;
-
+  inputs.zen-browser.packages.${pkgs.system}.generic
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";

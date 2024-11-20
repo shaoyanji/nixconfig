a0346ae0205d1342180ba0e77903bdaf360a78db
Shao-yan (Matt) Ji <100967396+shaoyanji@users.noreply.github.com>
2024-11-20 07:33:57 +0100

    nix-build: config update

--- modules/global/env.nix
+++ modules/global/env.nix
@@ -1,14 +1,55 @@
-{ config, pkgs, inputs, ... }:
+{ lib, config, pkgs, inputs, ... }:
 {
   imports = [ 
   ];
-  
+  home.activation.removeExistingGitConfig = lib.hm.dag.entryAfter ["checkLinkTargets"] ''
+    rm -f ~/.gitconfig
+  '';
   programs.git = {
     enable=true;
     userName="Shao-yan (Matt) Ji";
     userEmail="100967396+shaoyanji@users.noreply.github.com";
+    includes = [
+      {
+        path = "~/Documents/work/.gitconfig";
+        condition = "gitdir:~/Documents/work";
+      }
+    ];
     extraConfig = {
-      pull.rebase=false;
+      init.defaultBranch="main";
+      pull.rebase=true;
+      push.autoSetupRemote=true;
+    };
+    delta = {
+      enable=true;
+      options = {
+        side-by-side=true;
+        use-fancy-line-numbers=true;
+        highlight-renames=true;
+        highlight-old-new-lines=true;
+        highlight-new-old-lines=true;
+        line-numbers=true;
+      };
+    };
+    aliases = {
+      co="checkout";
+      ci="commit";
+      st="status";
+      br="branch";
+      hist="log --pretty=format:\"%h %ad | %s%d [%an]\" --graph --date=short";
+      amend="commit --amend";
+      undo="reset --soft HEAD~1";
+      unstage="reset HEAD";
+      last="log -1 HEAD";
+      last-n="log -n 1";
+      stage="add";
+      stage-all="add .";
+      unstage-all="reset";
+      stash="stash save";
+      pop="stash pop";
+      #   undo-last="reset HEAD~1";
+      sbupdate="submodule update --init --recursive";
+      sbforfetch="submodule foreach git fetch origin";
     };
   };
   home.sessionVariables = {

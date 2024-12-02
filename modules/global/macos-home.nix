c18f7a5bb58e0880334984fdaf97e6308b09f4e8
Shao-yan (Matt) Ji <100967396+shaoyanji@users.noreply.github.com>
2024-12-02 17:43:30 +0100

    nix-build: config update

--- modules/global/macos-home.nix
+++ modules/global/macos-home.nix
@@ -1,52 +1,23 @@
-{ pkgs, lib,  ... }:
+{ pkgs, lib, config, ... }:
 
 {
   home.stateVersion = "24.11";
   imports = [ 
     ./home.nix
-    # ./hyprland.nix
-    # ./lf
-    # ./env.nix 
-    # ./shell
-    # ./sops.nix
-    # ./nixvim 
-    # ./wezterm 
-    # ./kitty
-    # ./browser/firefox.nix
-    # ./nvim
-  ];
-
-
+    ];
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
-    # cmus
-    # glow
-    # entr
-    # gum
-    # go-task
-    # mc
-    # hyperfine
-    # tgpt
     # ]  ++ lib.optionals stdenv.isDarwin [
     # cocoapods
     # m-cli # useful macOS CLI commands
     # wezterm
   ];
   home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-   # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
+    #"Documents/Obsidian-Git-Sync".source = config.lib.file.mkOutOfStoreSymlink "/Volumes/FRITZ.NAS/External-USB3-0-01/documents/Obsidian-Git-Sync";
   };
 
     home.sessionVariables = {
-  #    EDITOR = "nvim";
-   };
+  };
     programs.home-manager.enable = true;
 }

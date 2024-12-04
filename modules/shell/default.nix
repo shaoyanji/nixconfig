767a3db3e1b038dc5124c2da4bcb985d3ed131c5
Shao-yan (Matt) Ji <100967396+shaoyanji@users.noreply.github.com>
2024-12-04 02:44:23 +0100

    nix-build: config update

--- modules/shell/default.nix
+++ modules/shell/default.nix
@@ -45,14 +45,16 @@ in
         eval "$(fzf --bash)"
         eval "$(zoxide init bash)"
         eval "$(starship init bash)"
-
       '';
     };
     starship = {
       enable = true;
       # Configuration written to ~/.config/starship.toml
       settings = {
-        # add_newline = false;
+        add_newline = false;
+        command_timeout = 1300;
+        scan_timeout = 50;
+        format= "$all$nix_shell$nodejs$lua$golang$rust$php$git_branch$git_commit$git_state$git_status\n$username$hostname$directory";
          character = {
            success_symbol = "[➜](bold green)";
            error_symbol = "[➜](bold red)";

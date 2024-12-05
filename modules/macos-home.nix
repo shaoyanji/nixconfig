f1cc2e5692f3ab4e4194706e72cf30ed3d6b05b2
Shao-yan (Matt) Ji <100967396+shaoyanji@users.noreply.github.com>
2024-12-05 20:36:47 +0100

    nix-build: config update

--- modules/macos-home.nix
+++ modules/macos-home.nix
@@ -7,7 +7,7 @@
 let  
     routerNAS = "/Volumes/FRITZ.NAS/External-USB3-0-01";
     routerShared = "/Volumes/Shared Library";
-    wolfNAS = "/Volumes/usbshare2"
+    wolfNAS = "/Volumes/usbshare2";
 in
 {
   home.stateVersion = "24.11";
@@ -25,7 +25,11 @@ in
   home.file = {
     "Documents/Obsidian-Git-Sync".source = config.lib.file.mkOutOfStoreSymlink "${routerNAS}/documents/Obsidian-Git-Sync";
     "Documents/work".source = config.lib.file.mkOutOfStoreSymlink "${routerNAS}/documents/work";
+    "nixconfig".source = config.lib.file.mkOutOfStoreSymlink "${routerNAS}/documents/nixconfig";
+    ".config/nix-darwin".source = config.lib.file.mkOutOfStoreSymlink "${wolfNAS}/configs/nixconfig";
     "Music/muzik".source = config.lib.file.mkOutOfStoreSymlink "${wolfNAS}/music/";
+    "Movies/video".source = config.lib.file.mkOutOfStoreSymlink "${wolfNAS}/video/";
+    "Pictures/pics".source = config.lib.file.mkOutOfStoreSymlink "${wolfNAS}/pics/";
     ".ollama/models".source = config.lib.file.mkOutOfStoreSymlink "${routerShared}/ollama";
     "go/pkg".source = config.lib.file.mkOutOfStoreSymlink "${routerShared}/go/pkg";
     ".config/btop".source = config.lib.file.mkOutOfStoreSymlink "${routerNAS}/dotfiles/btop";

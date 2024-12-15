8d7aa50807813a835fd1fdaa9a7c2f0ae137c35e
Shao-yan (Matt) Ji <100967396+shaoyanji@users.noreply.github.com>
2024-12-15 22:17:10 +0100

    nix-build: config update

--- modules/nixoshmsymlinks.nix
+++ modules/nixoshmsymlinks.nix
@@ -14,14 +14,15 @@ in
     "Documents/books".source = config.lib.file.mkOutOfStoreSymlink "${wolfNAS}/books";
     "Downloads/downloads".source = config.lib.file.mkOutOfStoreSymlink "${wolfNAS}/downloads";
     "Music/music".source = config.lib.file.mkOutOfStoreSymlink "${wolfNAS}/music";
-    "Pictures/pictures".source = config.lib.file.mkOutOfStoreSymlink "${wolfNAS}/pictures";
+    "Pictures/pictures".source = config.lib.file.mkOutOfStoreSymlink "${wolfNAS}/pics";
     "Videos/videos".source = config.lib.file.mkOutOfStoreSymlink "${wolfNAS}/video";
     "go/pkg".source = config.lib.file.mkOutOfStoreSymlink "${sharedNAS}/go/pkg";
-    ".cargo/registry".source = config.lib.file.mkOutOfStoreSymlink "${sharedNAS}/cargo/registry";
+    ".cargo/registry".source = config.lib.file.mkOutOfStoreSymlink "${sharedNAS}/.cargo/registry";
     ".ollama/models".source = config.lib.file.mkOutOfStoreSymlink "${sharedNAS}/ollama";
     #    ".zen".source = config.lib.file.mkOutOfStoreSymlink "${routerNAS}/dotfiles/zen";
     ".config/btop".source = config.lib.file.mkOutOfStoreSymlink "${routerNAS}/dotfiles/btop";
     ".config/cmus".source = config.lib.file.mkOutOfStoreSymlink "${routerNAS}/dotfiles/cmus";
     ".mozilla/firefox/profiles.ini".source = config.lib.file.mkOutOfStoreSymlink "${routerNAS}/dotfiles/firefox";
+    ".cloak/accounts.age".source = config.lib.file.mkOutOfStoreSymlink "${routerNAS}/documents/secrets/cloak/accounts.age";
   };
 }

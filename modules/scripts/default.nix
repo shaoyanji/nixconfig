393c27dc1e0dcd3860635160e5dbbeaed3f55b2c
Shao-yan (Matt) Ji <100967396+shaoyanji@users.noreply.github.com>
2025-01-11 14:09:21 +0100

    nix-build was successful on Linux poseidon 6.12.8 #1-NixOS SMP PREEMPT_DYNAMIC Thu Jan  2 09:34:26 UTC 2025 x86_64 GNU/Linux

--- modules/scripts/default.nix
+++ modules/scripts/default.nix
@@ -1,11 +1,17 @@
 { config, pkgs, ... }:
 
 {
+  imports = [
+        #    ./duckduck.nix
+  ];
   home.packages = with pkgs;
   [
-    (pkgs.writers.writePython3Bin "duckduck" {} /*python*/ ''
+    (pkgs.writers.writeLuaBin "hellolua" {} /*lua*/ ''
         print("hello world")
-      '')
+   '')
+    (pkgs.writers.writePython3Bin "hellopython" {} /*python*/ ''
+        print("hello world")
+   '')
   ];
   home.file = {
   };

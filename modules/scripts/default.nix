ba45dad6dd511caf072c0f1496b4829e632d6584
Shao-yan (Matt) Ji <100967396+shaoyanji@users.noreply.github.com>
2025-01-11 14:42:41 +0100

    nix-build was successful on Linux poseidon 6.12.8 #1-NixOS SMP PREEMPT_DYNAMIC Thu Jan  2 09:34:26 UTC 2025 x86_64 GNU/Linux

--- modules/scripts/default.nix
+++ modules/scripts/default.nix
@@ -5,6 +5,67 @@
         #    ./duckduck.nix
   ];
   home.packages = with pkgs;[
+    (pkgs.writers.writeLuaBin "duckduck" {} /*lua*/ ''
+-- Handle command-line arguments
+local args = {...}
+-- Handle piped input
+--local piped_input = io.stdin:read("*a")
+--if piped_input and piped_input ~= "" then
+--  print("Received piped input:", piped_input)
+--end
+-- Your main script logic here
+-- Fetch the x-vqd-4 header
+local handle = io.popen("curl -s -X GET https://duckduckgo.com/duckchat/v1/status -H 'x-vqd-accept: 1' -D -")
+local result = handle:read("*a")
+handle:close()
+-- Extract the x-vqd-4 value
+local vqd = result:match("x%-vqd%-4: ([^\r\n]+)")
+if vqd then
+    -- Use the extracted vqd in the POST request
+    local request = io.popen(string.format([[
+        curl -s -X POST 'https://duckduckgo.com/duckchat/v1/chat' \
+        -H 'x-vqd-4: %s' \
+        -H 'Content-Type: application/json' \
+        -H 'Accept: text/event-stream' \
+        -d '{
+            "model": "claude-3-haiku-20240307",
+            "messages": [{"role": "user", "content": "%s"}]
+        }' \
+        --no-buffer
+    ]], vqd, args[1]))
+
+    local response = request:read("*a")
+
+    request:close()
+
+-- Parse the JSON stream and extract messages
+    local messages = {}
+    for line in response:gmatch("[^\r\n]+") do
+        if line:match("^data:") then
+            local message = line:match('"message":"(.-)"')
+            if message then
+                message = message:gsub('\\"', '"') -- Unescape quotes
+                table.insert(messages, message)
+            end
+        end
+    end
+
+    -- Concatenate and print messages
+--    print("Extracted messages:")
+    print(table.concat(messages, ""))
+else
+
+--    print("Command output:\n" .. response)
+
+--else
+    print("Failed to extract x-vqd-4 header")
+end
+   '')
+    (pkgs.writers.writeRustBin "hellorust" {} /*rust*/ ''
+        fn main() {
+            println!("hello world");
+        }
+    '')
     (pkgs.writers.writeBashBin "hellobash" {} /*bash*/ ''
         echo "hello world"
     '')
@@ -26,7 +87,6 @@
     (pkgs.writers.writePython3Bin "hellopython" {} /*python*/ ''
         print("hello world")
     '')
-
   ];
   home.file = {
   };

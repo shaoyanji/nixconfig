3a1986fae57059c480ca6798cb91ac3f153bddb1
shaoyanji <jisifu@gmail.com>
2025-01-13 07:03:36 +0000

    tidy: config update

--- hosts/ares/configuration.nix
+++ hosts/ares/configuration.nix
@@ -6,11 +6,13 @@
       ./hardware-configuration.nix
       ../impermanence.nix
       ../base-desktop-environment.nix
-      ../minimal-desktop-environment.nix
+      ../minimal-desktop.nix
     ];
   networking.hostName = "ares"; # Define your hostname.
   environment.systemPackages = with pkgs; [
     firefox
+    inputs.zen-browser.packages.${pkgs.system}.twilight
+    qutebrowser
   ];
   system.stateVersion = "24.11"; # Did you read the comment?
 

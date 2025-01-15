df3c7f1cde7e042d94137aab5e6b438ac9f67d1c
Shao-yan (Matt) Ji <100967396+shaoyanji@users.noreply.github.com>
2025-01-15 11:28:50 +0100

    nix-build was successful on Linux lunarfall 6.12.4-400.asahi.fc41.aarch64+16k #1 SMP PREEMPT_DYNAMIC Thu Dec 12 10:57:58 UTC 2024 aarch64 GNU/Linux

--- modules/global/home.nix
+++ modules/global/home.nix
@@ -17,7 +17,7 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
-    nerd-fonts.jetbrains-mono
+    (pkgs.nerd-fonts.jetbrains-mono)
     wget
     gum
     go-task

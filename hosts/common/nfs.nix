d8a2e1006e9b4270f3c5640730c55410aecd4bbd
Shao-yan (Matt) Ji <100967396+shaoyanji@users.noreply.github.com>
2025-01-18 20:56:16 +0100

    nix-build was successful on Linux poseidon 6.12.9 #1-NixOS SMP PREEMPT_DYNAMIC Thu Jan  9 12:33:55 UTC 2025 x86_64 GNU/Linux

--- hosts/common/nfs.nix
+++ hosts/common/nfs.nix
@@ -4,4 +4,20 @@
     fsType = "nfs";
     options = [ "x-systemd.automount" "noauto" "x-systemd.after=network-online.target" "x-systemd.mount-timeout=30" ];
   };
+  #services.rpcbind.enable = true;
+  #systemd.mount = [{
+  #  type = "nfs";
+  #  mountConfig = {
+  #    Options = "noatime";
+  #  };
+  #  what = "192.168.178.4:/volume1/peachcable";
+  #  where = "/mnt/w";
+  #}]; 
+  #systemd.automount = [{
+  #  wantedBy = [ "multi-user.target" ];
+  #  automountConfig = {
+  #    TimeoutIdleSec = "30";
+  #  };
+  #  where = "/mnt/w";
+  #}];
 }

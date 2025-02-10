0b282e1ff7f68195dfa55c528b40ce4bc1386e5e
Shao-yan (Matt) Ji <100967396+shaoyanji@users.noreply.github.com>
2025-02-10 09:48:28 +0100

    nix-build was successful on Linux poseidon 6.13.1-cachyos #1-NixOS SMP PREEMPT_DYNAMIC Sat Feb  1 16:21:26 UTC 2025 x86_64 GNU/Linux

--- hosts/common/localmounts.nix
+++ hosts/common/localmounts.nix
@@ -1,58 +1,61 @@
-{config, pkgs,...}:   let 
-    burgernas = "100.72.61.23";
-    burgernas_nfs = "192.168.178.4";
-    fritznas = "192.168.178.1";
-    automount_opts = "x-systemd.automount,x-systemd.after=network-online.target,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=10s,x-systemd.mount-timeout=10s";
-    reg_opts = "rw,noserverino,uid=1000,gid=1";
-    tailscale_opts = "x-systemd.requires=tailscaled.service";
-    cred_wolf="credentials=${config.sops.secrets."server/localwd/credentials".path}";
-    cred_fritz="credentials=${config.sops.secrets."server/keyrepo/credentials".path}";
-  in
 {
-systemd.timers."burgernas-unmount" = {
-  wantedBy = [ "timers.target" ];
+  config,
+  pkgs,
+  ...
+}: let
+  burgernas = "100.72.61.23";
+  burgernas_nfs = "192.168.178.4";
+  #    fritznas = "192.168.178.1";
+  automount_opts = "x-systemd.automount,x-systemd.after=network-online.target,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=10s,x-systemd.mount-timeout=10s";
+  reg_opts = "rw,noserverino,uid=1000,gid=1";
+  tailscale_opts = "x-systemd.requires=tailscaled.service";
+  cred_wolf = "credentials=${config.sops.secrets."server/localwd/credentials".path}";
+  cred_fritz = "credentials=${config.sops.secrets."server/keyrepo/credentials".path}";
+in {
+  systemd.timers."burgernas-unmount" = {
+    wantedBy = ["timers.target"];
     timerConfig = {
       OnCalendar = "22:59:00";
       Unit = "burgernas-unmount.service";
     };
-};
-
-systemd.services."burgernas-unmount" = {
-  script = ''
-    set -eu
-    ${pkgs.coreutils}/bin/umount /mnt/{v,w,x,z}
-  '';
-  serviceConfig = {
-    Type = "oneshot";
-    User = "root";
   };
-};
+
+  systemd.services."burgernas-unmount" = {
+    script = ''
+      set -eu
+      ${pkgs.coreutils}/bin/umount /mnt/{v,w,x,z}
+    '';
+    serviceConfig = {
+      Type = "oneshot";
+      User = "root";
+    };
+  };
   environment.systemPackages = [pkgs.cifs-utils];
-  fileSystems."/mnt/w" = {
+  fileSystems."/Volumes/peachcable" = {
     device = "${burgernas_nfs}:/volume1/peachcable";
     fsType = "nfs";
-    options = [ "${automount_opts}" ];
+    options = ["${automount_opts}"];
   };
 
-  fileSystems."/mnt/y" = {
-    device = "//${fritznas}/fritz.nas/External-USB3-0-01/";
-    fsType = "cifs";
-    options = 
-    ["${automount_opts},${reg_opts},${cred_fritz}"];
-  };
-  fileSystems."/mnt/z" = {
-    device = "//${burgernas}/usbshare2";
-    fsType = "cifs";
-    options = ["${automount_opts},${reg_opts},${cred_wolf},${tailscale_opts}"];
-  };
-  fileSystems."/mnt/x" = {
-    device = "//${burgernas}/Shared Library";
-    fsType = "cifs";
-    options = ["${automount_opts},${reg_opts},${cred_wolf},${tailscale_opts}"];
-  };
-  fileSystems."/mnt/v" = {
-    device = "//${burgernas}/usbshare1";
-    fsType = "cifs";
-    options = ["${automount_opts},${reg_opts},${cred_wolf},${tailscale_opts}"];
-  };
+  #  fileSystems."/mnt/y" = {
+  #    device = "//${fritznas}/fritz.nas/External-USB3-0-01/";
+  #    fsType = "cifs";
+  #    options =
+  #    ["${automount_opts},${reg_opts},${cred_fritz}"];
+  #  };
+  #fileSystems."/mnt/z" = {
+  #  device = "//${burgernas}/usbshare2";
+  #  fsType = "cifs";
+  #  options = ["${automount_opts},${reg_opts},${cred_wolf},${tailscale_opts}"];
+  #};
+  #fileSystems."/mnt/x" = {
+  #  device = "//${burgernas}/Shared Library";
+  #  fsType = "cifs";
+  #  options = ["${automount_opts},${reg_opts},${cred_wolf},${tailscale_opts}"];
+  #};
+  #fileSystems."/mnt/v" = {
+  #  device = "//${burgernas}/usbshare1";
+  #  fsType = "cifs";
+  #  options = ["${automount_opts},${reg_opts},${cred_wolf},${tailscale_opts}"];
+  #};
 }
--- modules/nixoshmsymlinks.nix
+++ modules/nixoshmsymlinks.nix
@@ -11,9 +11,9 @@
 in {
   home.file = {
     #"nixconfig".source = config.lib.file.mkOutOfStoreSymlink "${wolfNAS}/projects/repo/nixconfig";
-    "Documents/Obsidian-Git-Sync".source = config.lib.file.mkOutOfStoreSymlink "${routerNAS}/documents/Obsidian-Git-Sync";
-    "Documents/work".source = config.lib.file.mkOutOfStoreSymlink "${routerNAS}/documents/work";
-    "Documents/nixconfig".source = config.lib.file.mkOutOfStoreSymlink "${routerNAS}/documents/nixconfig";
+    # "Documents/Obsidian-Git-Sync".source = config.lib.file.mkOutOfStoreSymlink "${routerNAS}/documents/Obsidian-Git-Sync";
+    # "Documents/work".source = config.lib.file.mkOutOfStoreSymlink "${routerNAS}/documents/work";
+    # "Documents/nixconfig".source = config.lib.file.mkOutOfStoreSymlink "${routerNAS}/documents/nixconfig";
     "Documents/docs".source = config.lib.file.mkOutOfStoreSymlink "${wolfNAS}/documents";
     "Documents/books".source = config.lib.file.mkOutOfStoreSymlink "${wolfNAS}/books";
     "Downloads/downloads".source = config.lib.file.mkOutOfStoreSymlink "${wolfNAS}/downloads";
@@ -27,7 +27,7 @@ in {
     ".ollama/models".source = config.lib.file.mkOutOfStoreSymlink "${peachNAS}/ollama";
     # ".zen".source = config.lib.file.mkOutOfStoreSymlink "${routerNAS}/dotfiles/zen";
     #".mozilla/firefox/profiles.ini".source = config.lib.file.mkOutOfStoreSymlink "${routerNAS}/dotfiles/firefox";
-    ".cloak/accounts.age".source = config.lib.file.mkOutOfStoreSymlink "${routerNAS}/documents/secrets/cloak/accounts.age";
+    #".cloak/accounts.age".source = config.lib.file.mkOutOfStoreSymlink "${routerNAS}/documents/secrets/cloak/accounts.age";
     ".cloak/key.txt".source = config.lib.file.mkOutOfStoreSymlink "${peachNAS}/security/key.txt";
     # ".cloak/accounts".source = config.lib.file.mkOutOfStoreSymlink "${sharedNAS}/security/accounts";
     "gokrazy/hello".source = config.lib.file.mkOutOfStoreSymlink "${peachNAS}/hello";

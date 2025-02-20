9a9dc1aedd4106d2113745a9410a738cf03ccce4
Shao-yan (Matt) Ji <100967396+shaoyanji@users.noreply.github.com>
2025-02-20 18:50:16 +0100

    nix-build was successful on Linux orb-cassini 6.12.15-orbstack-00304-gd0ddcf70447d #60 SMP Tue Feb 18 19:55:47 UTC 2025 aarch64 GNU/Linux

--- hosts/orb-cassini/custom.nix
+++ hosts/orb-cassini/custom.nix
@@ -1,7 +1,14 @@
-{
+{pkgs, ...}: {
   services.tailscale.enable = true;
   #virtualisation.docker.rootless = {
   #  enable = true;
   #  setSocketVariable = true;
   #};
+  #
+
+  programs.nix-ld.enable = true;
+  programs.nix-ld.libraries = with pkgs; [
+    #libGL
+    alsa-lib
+  ];
 }

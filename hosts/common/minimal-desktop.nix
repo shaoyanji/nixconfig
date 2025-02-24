67b0d879032522f30de8795e35844ce762dd214d
Shao-yan (Matt) Ji <100967396+shaoyanji@users.noreply.github.com>
2025-02-24 16:58:01 +0100

    nix-build was successful on thinclient

--- hosts/common/minimal-desktop.nix
+++ hosts/common/minimal-desktop.nix
@@ -12,7 +12,7 @@
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKglnT7aVqb9CrFE0U/Y/ZFN8apH7urMXM2Sn9bJ23YV nixos@nixos"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHfnYbTF2d4ZA4+wKbXFL/UUJTcMqdLDhhAZZDP2KrPD devji@orb-cassini"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKubsmOU3nRDwO6gsqnYzORtUxvpwPDLNRAA/K99Xaqs devji@minyx"
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBKghahPOEp4Tj2deMlN3gjpdnzOnN78syiwmx4gUZSU devji@thinsandy"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILFrpuKNvTGARCr6vacmyHRkjDnfEbdAp9+zWHTRRDD9 devji@thinsandy"
   ];
 in {
   imports = [
--- hosts/thinsandy/configuration.nix
+++ hosts/thinsandy/configuration.nix
@@ -9,18 +9,22 @@
     ./hardware-configuration.nix
     ../common/minimal-desktop.nix
   ];
-
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
   # Use the GRUB 2 boot loader.
-  boot.loader.grub.enable = true;
+  #boot.loader.grub.enable = true;
   # boot.loader.grub.efiSupport = true;
   # boot.loader.grub.efiInstallAsRemovable = true;
   # boot.loader.efi.efiSysMountPoint = "/boot/efi";
   # Define on which hard drive you want to install Grub.
-  boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
+  #boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
 
   networking.hostName = "thinsandy"; # Define your hostname.
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  environment.systemPackages = with pkgs; [
+    btrfs-progs
+  ];
 
   # Set your time zone.
 

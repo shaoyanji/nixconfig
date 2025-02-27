a865d0d7a3d6d872716f1d90e0a67e8b0e2c3c9f
Shao-yan (Matt) Ji <100967396+shaoyanji@users.noreply.github.com>
2025-02-27 12:33:26 +0100

    tidy: config update

--- modules/shell/default.nix
+++ modules/shell/default.nix
@@ -26,21 +26,25 @@ in {
     bash = {
       shellAliases = myAliases;
     };
-    #    starship = {
-    #      enable = true;
-    #      # Configuration written to ~/.config/starship.toml
-    #      settings = {
-    #        add_newline = false;
-    #        command_timeout = 1300;
-    #        scan_timeout = 50;
-    #        format= /*bash*/"$all$nix_shell$nodejs$lua$golang$rust$php$git_branch$git_commit$git_state$git_status\n$username$hostname$directory";
-    #         character = {
-    #           success_symbol = "[➜](bold green)";
-    #           error_symbol = "[➜](bold red)";
-    #         };
-    #        # package.disabled = true;
-    #      };
-    #    };
+    starship = {
+      enable = true;
+      # Configuration written to ~/.config/starship.toml
+      settings = {
+        add_newline = false;
+        command_timeout = 1300;
+        scan_timeout = 50;
+        format =
+          /*
+          bash
+          */
+          "$all$nix_shell$nodejs$lua$golang$rust$php$git_branch$git_commit$git_state$git_status\n$username$hostname$directory";
+        character = {
+          success_symbol = "[➜](bold green)";
+          error_symbol = "[➜](bold red)";
+        };
+        # package.disabled = true;
+      };
+    };
     direnv = {
       enable = true;
       nix-direnv.enable = true;

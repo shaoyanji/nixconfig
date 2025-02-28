14d7c37c7e4f49a9acac08390bbee2cefc4ab1b7
Shao-yan (Matt) Ji <100967396+shaoyanji@users.noreply.github.com>
2025-02-28 13:31:34 +0100

    nix-build was successful on Linux ancientace 6.13.3 #1-NixOS SMP PREEMPT_DYNAMIC Mon Feb 17 10:36:59 UTC 2025 x86_64 GNU/Linux

--- hosts/ancientace/configuration.nix
+++ hosts/ancientace/configuration.nix
@@ -45,9 +45,19 @@
       enable = true;
       role = "server"; # Or "agent" for worker only nodes
       tokenFile = "${config.sops.secrets."local/k3s/token".path}";
-      serverAddr = "https://thinsandy:6443";
+      serverAddr = "https://thinsandy.cloudforest-kardashev.ts.net:6443";
     };
   };
+
+  networking.firewall.allowedTCPPorts = [
+    6443 # k3s: required so that pods can reach the API server (running on port 6443 by default)
+    # 2379 # k3s, etcd clients: required if using a "High Availability Embedded etcd" configuration
+    # 2380 # k3s, etcd peers: required if using a "High Availability Embedded etcd" configuration
+  ];
+
+  networking.firewall.allowedUDPPorts = [
+    # 8472 # k3s, flannel: required if using multi-node for inter-node networking
+  ];
   environment.systemPackages = with pkgs; [
     wget
     git

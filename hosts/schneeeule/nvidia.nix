6899bca1653967777807c396844499d916846d4e
Shao-yan (Matt) Ji <100967396+shaoyanji@users.noreply.github.com>
2025-02-28 22:09:43 +0100

    schneeeule fix

--- hosts/schneeeule/nvidia.nix
+++ hosts/schneeeule/nvidia.nix
@@ -16,10 +16,10 @@
   };
   nixpkgs.config.allowBroken = true;
 
-  #boot.kernelPackages = lib.mkForce pkgs.linuxPackages_cachyos;
+  boot.kernelPackages = lib.mkForce pkgs.linuxPackages_cachyos;
   hardware.nvidia = {
-    #package = config.boot.kernelPackages.nvidiaPackages.legacy_470;
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    package = config.boot.kernelPackages.nvidiaPackages.legacy_470;
+    #package = config.boot.kernelPackages.nvidiaPackages.stable;
     prime = {
       nvidiaBusId = "PCI:9:0:0";
       intelBusId = "PCI:0:2:0";

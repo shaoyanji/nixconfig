552d01368938461e142733c23ec565eb126b687c
Shao-yan (Matt) Ji <100967396+shaoyanji@users.noreply.github.com>
2025-03-01 00:25:03 +0100

    nix-build was successful on Linux thinsandy 6.13.3 #1-NixOS SMP PREEMPT_DYNAMIC Mon Feb 17 10:36:59 UTC 2025 x86_64 GNU/Linux

--- hosts/thinsandy/configuration.nix
+++ hosts/thinsandy/configuration.nix
@@ -32,20 +32,20 @@
 
   virtualisation.docker.enable = true;
 
-  services.k3s = {
-    enable = true;
-    role = "server";
-    tokenFile = "${config.sops.secrets."local/k3s/token".path}";
-    clusterInit = true;
-  };
-  networking.firewall.allowedTCPPorts = [
-    6443 # k3s: required so that pods can reach the API server (running on port 6443 by default)
-    2379 # k3s, etcd clients: required if using a "High Availability Embedded etcd" configuration
-    2380 # k3s, etcd peers: required if using a "High Availability Embedded etcd" configuration
-  ];
+  #services.k3s = {
+  #  enable = true;
+  #  role = "server";
+  #  tokenFile = "${config.sops.secrets."local/k3s/token".path}";
+  #  clusterInit = true;
+  #};
+  #networking.firewall.allowedTCPPorts = [
+  #  6443 # k3s: required so that pods can reach the API server (running on port 6443 by default)
+  #  2379 # k3s, etcd clients: required if using a "High Availability Embedded etcd" configuration
+  #  2380 # k3s, etcd peers: required if using a "High Availability Embedded etcd" configuration
+  #];
 
-  networking.firewall.allowedUDPPorts = [
-    8472 # k3s, flannel: required if using multi-node for inter-node networking
-  ];
+  #networking.firewall.allowedUDPPorts = [
+  #  8472 # k3s, flannel: required if using multi-node for inter-node networking
+  #];
   system.stateVersion = "24.11"; # Did you read the comment?
 }

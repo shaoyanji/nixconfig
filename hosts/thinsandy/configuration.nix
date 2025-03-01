7a2532bac27c4fc76709d67e6dc88a9b487b8477
Shao-yan (Matt) Ji <100967396+shaoyanji@users.noreply.github.com>
2025-03-01 01:33:08 +0100

    nix-build was successful on Linux thinsandy 6.13.3 #1-NixOS SMP PREEMPT_DYNAMIC Mon Feb 17 10:36:59 UTC 2025 x86_64 GNU/Linux

--- hosts/thinsandy/configuration.nix
+++ hosts/thinsandy/configuration.nix
@@ -40,8 +40,8 @@
   };
   networking.firewall.allowedTCPPorts = [
     6443 # k3s: required so that pods can reach the API server (running on port 6443 by default)
-    2379 # k3s, etcd clients: required if using a "High Availability Embedded etcd" configuration
-    2380 # k3s, etcd peers: required if using a "High Availability Embedded etcd" configuration
+    #    2379 # k3s, etcd clients: required if using a "High Availability Embedded etcd" configuration
+    #    2380 # k3s, etcd peers: required if using a "High Availability Embedded etcd" configuration
   ];
 
   networking.firewall.allowedUDPPorts = [

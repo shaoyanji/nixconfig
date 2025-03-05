d0f9d27a492e339fee08dd6574d5296dbdb11458
Shao-yan (Matt) Ji <100967396+shaoyanji@users.noreply.github.com>
2025-03-05 19:16:24 +0100

    nix-build was successful on Linux schneeeule 6.12.14 #1-NixOS SMP PREEMPT_DYNAMIC Mon Feb 17 09:05:48 UTC 2025 x86_64 GNU/Linux

--- hosts/schneeeule/nvidia.nix
+++ hosts/schneeeule/nvidia.nix
@@ -8,15 +8,17 @@
     ../common/nvidia.nix
   ];
 
+  extraPackages = with pkgs; [nvidia-vaapi-driver];
   services.xserver.videoDrivers = ["intel"];
   nixpkgs.config = {
     allowUnfree = true;
     nvidia.acceptLicense = true;
     cudaSupport = true; # Enables CUDA support
   };
-  nixpkgs.config.allowBroken = true;
+  #  nixpkgs.config.allowBroken = true;
 
-  boot.kernelPackages = lib.mkForce pkgs.linuxPackages;
+  #  boot.kernelPackages = lib.mkForce pkgs.linuxPackages;
+  boot.kernelPackages = lib.mkForce pkgs.linuxPackages_cachyos;
   hardware.nvidia = {
     package = config.boot.kernelPackages.nvidiaPackages.legacy_470;
     #package = config.boot.kernelPackages.nvidiaPackages.stable;

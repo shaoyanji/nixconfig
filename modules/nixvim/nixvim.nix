8143fae1edf20caed0bcd784c9cbcfb9358afcaa
Shao-yan (Matt) Ji <100967396+shaoyanji@users.noreply.github.com>
2025-04-28 15:27:26 +0200

    nix-build was successful on Linux poseidon 6.14.2-cachyos #1-NixOS SMP PREEMPT_DYNAMIC Thu Apr 10 12:44:49 UTC 2025 x86_64 GNU/Linux

--- modules/nixvim/nixvim.nix
+++ modules/nixvim/nixvim.nix
@@ -133,11 +133,11 @@
       #   https://nix-community.github.io/nixvim/colorschemes/tokyonight/index.html
       tokyonight = {
         enable = true;
-        settings = {
-          # Like many other themes, this one has different styles, and you could load
-          # any other, such as 'storm', 'moon', or 'day'.
-          style = "night";
-        };
+        # settings = {
+        # Like many other themes, this one has different styles, and you could load
+        # any other, such as 'storm', 'moon', or 'day'.
+        #    style = "night";
+        #   };
       };
     };
 

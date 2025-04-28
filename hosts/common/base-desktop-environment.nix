38893016c32c5ef97e0438665f4049208e3b8976
Shao-yan (Matt) Ji <100967396+shaoyanji@users.noreply.github.com>
2025-04-28 15:38:57 +0200

    nix-build was successful on Linux poseidon 6.14.2-cachyos #1-NixOS SMP PREEMPT_DYNAMIC Thu Apr 10 12:44:49 UTC 2025 x86_64 GNU/Linux

--- hosts/common/base-desktop-environment.nix
+++ hosts/common/base-desktop-environment.nix
@@ -57,7 +57,7 @@
   environment.systemPackages = with pkgs; [
     kitty
     ghostty
-    inputs.zen-browser.packages.${stdenv.hostPlatform.system}.twilight-official
+    inputs.zen-browser.packages.${stdenv.hostPlatform.system}.twilight
     qutebrowser
     lan-mouse_git
     nix-top

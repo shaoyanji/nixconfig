a31dc73a849906fd50eb268f573d05efe5b2d5dc
Shao-yan (Matt) Ji <100967396+shaoyanji@users.noreply.github.com>
2025-04-28 15:43:30 +0200

    nix-build was successful on Linux poseidon 6.14.2-cachyos #1-NixOS SMP PREEMPT_DYNAMIC Thu Apr 10 12:44:49 UTC 2025 x86_64 GNU/Linux

--- modules/nixvim/nixvim.nix
+++ modules/nixvim/nixvim.nix
@@ -129,17 +129,17 @@
     # Don't forget to disable the colorschemes you arent using
     #
     # If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    colorschemes = {
-      #   https://nix-community.github.io/nixvim/colorschemes/tokyonight/index.html
-      tokyonight = {
-        enable = true;
-        # settings = {
-        # Like many other themes, this one has different styles, and you could load
-        # any other, such as 'storm', 'moon', or 'day'.
-        #    style = "night";
-        #   };
-      };
-    };
+    #colorschemes = {
+    #   https://nix-community.github.io/nixvim/colorschemes/tokyonight/index.html
+    # tokyonight = {
+    #   enable = true;
+    # settings = {
+    # Like many other themes, this one has different styles, and you could load
+    # any other, such as 'storm', 'moon', or 'day'.
+    #    style = "night";
+    #   };
+    # };
+    #};
 
     # https://nix-community.github.io/nixvim/NeovimOptions/index.html?highlight=globals#globals
     globals = {

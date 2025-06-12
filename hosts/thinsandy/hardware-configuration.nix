741bb95fff94e4b3fa5f0b570bea3e9685f80019
Shao-yan (Matt) Ji <100967396+shaoyanji@users.noreply.github.com>
2025-06-12 16:34:52 +0200

    nix-build was successful on Linux thinsandy 6.15.1 #1-NixOS SMP PREEMPT_DYNAMIC Wed Jun  4 12:46:27 UTC 2025 x86_64 GNU/Linux

--- hosts/thinsandy/hardware-configuration.nix
+++ hosts/thinsandy/hardware-configuration.nix
@@ -10,7 +10,7 @@
 }: {
   imports = [
     # ../common/btrfsautoscrub.nix
-    ../common/samba.nix
+    # ../common/samba.nix
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
 
@@ -79,6 +79,69 @@
     options = ["bind"];
   };
 
+  services.nfs.server.enable = true;
+  services.nfs.server.exports = ''
+    /export 192.168.178.0/24(rw,fsid=0,no_subtree_check)
+    /export/data 192.168.178.0/24(rw,async,no_wdelay,hide,crossmnt,no_subtree_check,insecure_locks,anonuid=1000,anongid=100,sec=sys,insecure,root_squash,all_squash)
+    /export/data 100.66.146.18(rw,async,no_wdelay,hide,crossmnt,no_subtree_check,insecure_locks,anonuid=1000,anongid=100,sec=sys,insecure,root_squash,all_squash)
+  '';
+  services.samba = {
+    enable = true;
+    #  securityType = "user";
+    openFirewall = true;
+    settings = {
+      global = {
+        "workgroup" = "WORKGROUP";
+        "server string" = "smbnix";
+        "netbios name" = "smbnix";
+        "security" = "user";
+        #"use sendfile" = "yes";
+        #"max protocol" = "smb2";
+        # note: localhost is the ipv6 localhost ::1
+        #"hosts allow" = "192.168.178.0 ";
+        #"hosts deny" = "0.0.0.0/0";
+        #"guest account" = "nobody";
+        #"map to guest" = "bad user";
+      };
+      "data" = {
+        "path" = "/export/data";
+        "browseable" = "yes";
+        "read only" = "no";
+        "guest ok" = "yes";
+        "create mask" = "0644";
+        "directory mask" = "0755";
+        #"force user" = "devji";
+        #"force group" = "";
+      };
+      "private" = {
+        "path" = "/export/private";
+        "browseable" = "yes";
+        "read only" = "no";
+        "guest ok" = "no";
+        "create mask" = "0644";
+        "directory mask" = "0755";
+      };
+
+      "public" = {
+        "path" = "/export/public";
+        "browseable" = "yes";
+        "read only" = "no";
+        "guest ok" = "no";
+        "create mask" = "0644";
+        "directory mask" = "0755";
+      };
+    };
+  };
+
+  services.samba-wsdd = {
+    enable = true;
+    openFirewall = true;
+  };
+
+  networking.firewall.allowedTCPPorts = [445 139 2049];
+  networking.firewall.allowedUDPPorts = [137 138];
+  networking.firewall.enable = true;
+  networking.firewall.allowPing = true;
   services.btrfs.autoScrub = {
     enable = true;
     interval = "monthly";

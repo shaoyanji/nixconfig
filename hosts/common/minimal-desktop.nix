8a7f58b9d518d67b5ed28a09215973522ce58b57
Shao-yan (Matt) Ji <100967396+shaoyanji@users.noreply.github.com>
2025-06-14 17:50:44 +0200

    nix-build was successful on Linux thinsandy 6.15.1 #1-NixOS SMP PREEMPT_DYNAMIC Wed Jun  4 12:46:27 UTC 2025 x86_64 GNU/Linux

--- hosts/common/minimal-desktop.nix
+++ hosts/common/minimal-desktop.nix
@@ -17,6 +17,7 @@
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICW0tS7KTBySuE49gSHuqAK1Je+ODY+YGc81eTIvonFh Shortcuts on cassini"
     # "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICi5cT/V0vDAwTQKtYAVRZR6BSEvNAfZX+M/3wDrd0nb dietpi@dietrichpimpernel"
     "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBJB2LmOgmY+pLU/QGf0KZUoujlklRin7XJoPN0DFBSC4e6ivsH/Uaqa3pfmsOx8vUHef2492cnWUt5BFn7cJPrk="
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIZXtw+dZv/J1zRWjeyQS7NzLO53vjHfDRIBL5k1anMg jisifu@gmail.com"
   ];
 in {
   imports = [

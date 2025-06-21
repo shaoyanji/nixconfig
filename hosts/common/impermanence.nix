5a5d26e014256e4decce713c5b9b87c145cdf21f
Shao-yan (Matt) Ji <100967396+shaoyanji@users.noreply.github.com>
2025-06-22 00:17:39 +0200

    nix-build was successful on Linux schneeeule 6.12.33 #1-NixOS SMP PREEMPT_DYNAMIC Tue Jun 10 11:13:00 UTC 2025 x86_64 GNU/Linux

--- hosts/common/impermanence.nix
+++ hosts/common/impermanence.nix
@@ -42,7 +42,7 @@
       "/var/lib/systemd/coredump"
       "/var/lib/tailscale"
       "/etc/NetworkManager/system-connections"
-      "/var/lib/ollama"
+      "/var/lib/private/ollama"
       "/etc/rancher/k3s"
       "/var/lib/rancher"
       {

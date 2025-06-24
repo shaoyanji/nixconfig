8a1472dbb20c3253d72c553c1a97ae061f1d6316
Shao-yan (Matt) Ji <100967396+shaoyanji@users.noreply.github.com>
2025-06-24 11:31:38 +0200

    tidy: config update from schneeeule

--- hosts/common/minimal-desktop.nix
+++ hosts/common/minimal-desktop.nix
@@ -18,6 +18,7 @@
     # "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICi5cT/V0vDAwTQKtYAVRZR6BSEvNAfZX+M/3wDrd0nb dietpi@dietrichpimpernel"
     "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBJB2LmOgmY+pLU/QGf0KZUoujlklRin7XJoPN0DFBSC4e6ivsH/Uaqa3pfmsOx8vUHef2492cnWUt5BFn7cJPrk="
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIZXtw+dZv/J1zRWjeyQS7NzLO53vjHfDRIBL5k1anMg jisifu@gmail.com"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOD4PopDAxzh1t4nNnDE/xiWLGYzopLRzZ7eBwd4hHza devji@schneeeule"
   ];
 in {
   imports = [

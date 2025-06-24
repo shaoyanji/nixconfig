9a8f11c2d2c9ffb001ff71f0d72edc81eca25c50
Shao-yan (Matt) Ji <100967396+shaoyanji@users.noreply.github.com>
2025-06-24 12:43:23 +0200

    nix-build was successful on Linux schneeeule 6.12.33 #1-NixOS SMP PREEMPT_DYNAMIC Tue Jun 10 11:13:00 UTC 2025 x86_64 GNU/Linux

--- hosts/schneeeule/nvidia.nix
+++ hosts/schneeeule/nvidia.nix
@@ -23,7 +23,7 @@
     package = config.boot.kernelPackages.nvidiaPackages.legacy_470;
     #package = config.boot.kernelPackages.nvidiaPackages.stable;
     prime = {
-      nvidiaBusId = "PCI:9:0:0";
+      nvidiaBusId = "PCI:10:0:0";
       intelBusId = "PCI:0:2:0";
     };
   };

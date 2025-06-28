b77a99890ce6a076743b1e128f44e0847fdc02d0
Shao-yan (Matt) Ji <100967396+shaoyanji@users.noreply.github.com>
2025-06-28 17:30:11 +0200

    tidy: config update from penguin

--- modules/global/global.nix
+++ modules/global/global.nix
@@ -26,6 +26,7 @@
       "https://nix-gaming.cachix.org"
       "https://cache.lix.systems"
       "https://cuda-maintainers.cachix.org"
+      "https://cache.garnix.io"
     ];
     trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
@@ -38,6 +39,7 @@
       "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
       "cache.lix.systems:aBnZUw8zA7H35Cz2RyKFVs3H4PlGTLawyY5KRbvJR8o="
       "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
     ];
     trusted-users = [
       "@admin"

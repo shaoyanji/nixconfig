00df306f003ba46697aee6c9a70537fe94b943d0
Shao-yan (Matt) Ji <100967396+shaoyanji@users.noreply.github.com>
2025-09-24 12:35:08 +0200

    git update check

--- hosts/poseidon/nvidia.nix
+++ hosts/poseidon/nvidia.nix
@@ -3,7 +3,7 @@
     ../common/nvidia.nix
   ];
   hardware.nvidia = {
-    package = config.boot.kernelPackages.nvidiaPackages.beta;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
     prime = {
       nvidiaBusId = "PCI:1:0:0";
       amdgpuBusId = "PCI:5:0:0";

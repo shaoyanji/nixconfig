2d8b813dcb73e270f6e4a495200d83cb11967aa9
Shao-yan (Matt) Ji <e@mail.shaoyanji@is-a.dev>
2025-11-20 22:59:26 +0100

    tidy: config update from cassini

--- modules/caelestia.nix
+++ modules/caelestia.nix
@@ -1,23 +1,21 @@
 {
-  home = {
-    programs.caelestia = {
-      enable = true;
-      systemd = {
-        enable = false; # if you prefer starting from your compositor
-        target = "graphical-session.target";
-        environment = [];
+  programs.caelestia = {
+    enable = true;
+    systemd = {
+      enable = false; # if you prefer starting from your compositor
+      target = "graphical-session.target";
+      environment = [];
+    };
+    settings = {
+      bar.status = {
+        showBattery = false;
       };
+      paths.wallpaperDir = "~/Pictures/Wallpapers";
+    };
+    cli = {
+      enable = true; # Also add caelestia-cli to path
       settings = {
-        bar.status = {
-          showBattery = false;
-        };
-        paths.wallpaperDir = "~/Pictures/Wallpapers";
-      };
-      cli = {
-        enable = true; # Also add caelestia-cli to path
-        settings = {
-          theme.enableGtk = false;
-        };
+        theme.enableGtk = false;
       };
     };
   };

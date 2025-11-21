bf2c77de97a784d7ae04af268c46bec6c2ea3e7c
Shao-yan (Matt) Ji <e@mail.shaoyanji@is-a.dev>
2025-11-21 13:47:38 +0100

    thinsandy 106 2025-11-21 a4a95ebdf7919b0ecbd13c4d2f3e7ed35f957995-dirty

--- hosts/thinsandy/configuration.nix
+++ hosts/thinsandy/configuration.nix
@@ -127,7 +127,24 @@
   };
   users.users.immich.extraGroups = ["video" "render"];
   services = {
+    nginx.virtualHosts."nixos.servebeer.com" = {
+      enableACME = true;
+      forceSSL = true;
+      locations."/" = {
+        proxyPass = "http://[::1]:${toString config.services.immich.port}";
+        proxyWebsockets = true;
+        recommendedProxySettings = true;
+        extraConfig = ''
+          client_max_body_size 50000M;
+          proxy_read_timeout   600s;
+          proxy_send_timeout   600s;
+          send_timeout         600s;
+        '';
+      };
+    };
     immich = {
+      host = "0.0.0.0";
+      mediaLocation = /srv/public/;
       enable = true;
       port = 2283;
       accelerationDevices = null;

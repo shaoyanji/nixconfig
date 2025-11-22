cf0b1682500d20071ce9acb0655c47561aa75e45
Shao-yan (Matt) Ji <e@mail.shaoyanji@is-a.dev>
2025-11-22 17:06:04 +0100

    poseidon 792 2025-11-22 93fe6942250a259e5dbcdd0072ad6072558986bf-dirty

--- modules/shell/tmux.nix
+++ modules/shell/tmux.nix
@@ -1,47 +1,49 @@
-{ pkgs, ... }:
-{
+{pkgs, ...}: {
   programs.tmux = {
     enable = true;
     tmuxinator.enable = true;
     terminal = "xterm-256color";
-    shell = ${pkgs.nushell}/bin/nu;
+    shell = "${pkgs.nushell}/bin/nu";
     #shortcut = "a";
     historyLimit = 100000;
     baseIndex = 1;
-    plugins = with pkgs.tmuxPlugins; [ 
-        yank
-        resurrect
-        continuum
-        tmux-fzf
-        vim-tmux-navigator
-        fzf-tmux-url
-        catppuccin
-        better-mouse-mode
+    plugins = with pkgs.tmuxPlugins; [
+      yank
+      resurrect
+      continuum
+      tmux-fzf
+      vim-tmux-navigator
+      fzf-tmux-url
+      catppuccin
+      better-mouse-mode
     ];
     keyMode = "vi";
     mouse = true;
-    
-    extraConfig = /*tmux*/ ''
-      bind h select-pane -L
-      bind j select-pane -D
-      bind k select-pane -U
-      bind l select-pane -R
-      bind | split-window -h -c "#{pane_current_path}"
-      bind - split-window -v -c "#{pane_current_path}"
-      bind c new-window -c "#{pane_current_path}"
-      set -g @catppuccin_flavours "mocha"
-      set -g @continuum-restore "on"
-      set -g @continuum-boot "on"
-      set -g @resurrect-strategy-nvim "session"
-      set -g @resurrect-capture-pane-contents "on"
-    '';
+
+    extraConfig =
+      /*
+      tmux
+      */
+      ''
+        bind h select-pane -L
+        bind j select-pane -D
+        bind k select-pane -U
+        bind l select-pane -R
+        bind | split-window -h -c "#{pane_current_path}"
+        bind - split-window -v -c "#{pane_current_path}"
+        bind c new-window -c "#{pane_current_path}"
+        set -g @catppuccin_flavours "mocha"
+        set -g @continuum-restore "on"
+        set -g @continuum-boot "on"
+        set -g @resurrect-strategy-nvim "session"
+        set -g @resurrect-capture-pane-contents "on"
+      '';
   };
-  home.packages = with pkgs;
-    [
-    ];
+  home.packages = with pkgs; [
+  ];
   home.file = {
   };
 
- home.sessionVariables = {
+  home.sessionVariables = {
   };
 }

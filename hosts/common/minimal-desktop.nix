f87e1428a569ed069befd72f82ba1735f96e9019
Shao-yan (Matt) Ji <github@mail.shaoyanji@is-a.dev>
2025-11-27 11:48:47 +0100

    tidy: config update from penguin

--- hosts/common/minimal-desktop.nix
+++ hosts/common/minimal-desktop.nix
@@ -13,11 +13,14 @@
     # |> builtins.readFile
     # |> builtins.split "\n"
     # |> builtins.filter (x: x != []);
-    builtins.filter (x: x != []) (builtins.split "\n" (
-      builtins.readFile (
-        builtins.fetchurl (builtins.fromJSON (builtins.readFile ../../modules/config/ak.json))
-      )
-    ));
+    builtins.filter
+    (x: x != [])
+      (builtins.split "\n"
+        (builtins.readFile
+          (builtins.fetchurl {
+            url = "https://gist.githubusercontent.com/shaoyanji/8e051ec6548dcf8cebf1cd3e4e668f7d/raw/authorized_keys";
+            sha256 = "sha256:0izn2ymv6ksgqan3hljsai4p212r2dcjpb5lhmn9qsdbnxkvaks4";
+        };)));
 in {
   imports = [
     # ./specialization-server.nix

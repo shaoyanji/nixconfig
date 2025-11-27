bd564529df3973fc94ddf55333da8b46c56e9df7
Shao-yan (Matt) Ji <github@mail.shaoyanji@is-a.dev>
2025-11-27 11:58:11 +0100

    tidy: config update from penguin

--- hosts/garnixMachine.nix
+++ hosts/garnixMachine.nix
@@ -8,12 +8,18 @@
   sshKeys =
     builtins.filter
     (x: x != [])
-      (builtins.split "\n"
-        (builtins.readFile
-          (builtins.fetchurl {
+    (
+      builtins.split "\n"
+      (
+        builtins.readFile
+        (
+          builtins.fetchurl {
             url = "https://gist.githubusercontent.com/shaoyanji/8e051ec6548dcf8cebf1cd3e4e668f7d/raw/authorized_keys";
             sha256 = "sha256:0izn2ymv6ksgqan3hljsai4p212r2dcjpb5lhmn9qsdbnxkvaks4";
-          };)));
+          }
+        )
+      )
+    );
   backendPort = "3000";
 in {
   # This sets up networking and filesystems in a way that works with garnix

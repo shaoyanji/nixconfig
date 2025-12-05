559c891148599df646728752951372fa796361e2
Shao-yan (Matt) Ji <github@mail.shaoyanji@is-a.dev>
2025-12-06 00:09:15 +0100

    removed loopback again

--- hosts/common/base-desktop-environment.nix
+++ hosts/common/base-desktop-environment.nix
@@ -13,7 +13,7 @@
   ];
   # Bootloader.
   boot = {
-    extraModulePackages = with config.boot.kernelPackages; [v4l2loopback];
+    # extraModulePackages = with config.boot.kernelPackages; [v4l2loopback];
     #kernelModules = [
     #  "v4l2loopback"
     #];

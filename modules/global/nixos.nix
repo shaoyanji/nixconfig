67c92c89b84d43095a11d74dda2353d45ec4ef8d
Shao-yan (Matt) Ji <github@mail.shaoyanji@is-a.dev>
2025-12-05 23:06:22 +0100

    moved dank mat shell to home manager?

--- modules/global/nixos.nix
+++ modules/global/nixos.nix
@@ -12,12 +12,30 @@
       imports = [
         ./heim.nix
       ];
+      home.programs.dankMaterialShell = {
+        enable = true;
+        systemd = {
+          enable = true; # Systemd service for auto-start
+          restartIfChanged = true; # Auto-restart dms.service when dankMaterialShell changes
+        };
+        niri = {
+          enableKeybinds = true; # Automatic keybinding configuration
+          enableSpawn = true; # Auto-start DMS with niri
+        };
+        default.settings = {
+          theme = "dark";
+          dynamicTheming = true;
+          # Add any other settings here
+        };
+      };
     };
     sharedModules = [
       inputs.sops-nix.homeManagerModules.sops
       inputs.kickstart-nixvim.homeManagerModules.default
       inputs.nix-index-database.homeModules.nix-index
       # inputs.niri.homeModules.niri
+      inputs.dankMaterialShell.homeModules.dankMaterialShell.default
+      inputs.dankMaterialShell.homeModules.dankMaterialShell.niri
     ];
     extraSpecialArgs = {inherit inputs;}; # Pass inputs to homeManagerConfiguration
   };

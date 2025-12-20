af891d633701c12d969a8b20277759a763af82fa
Shao-yan (Matt) Ji <github@mail.shaoyanji@is-a.dev>
2025-12-20 17:33:46 +0100

    removed some of the dependencies taht are already included in nixpkgs

--- modules/niri.nix
+++ modules/niri.nix
@@ -130,14 +130,14 @@
     # };
     # Core features
     enableSystemMonitoring = true; # System monitoring widgets (dgop)
-    enableClipboard = true; # Clipboard history manager
+    # enableClipboard = true; # Clipboard history manager
     enableVPN = true; # VPN management widget
-    enableBrightnessControl = true; # Backlight/brightness controls
-    enableColorPicker = true; # Color picker tool
+    # enableBrightnessControl = true; # Backlight/brightness controls
+    # enableColorPicker = true; # Color picker tool
     enableDynamicTheming = true; # Wallpaper-based theming (matugen)
     enableAudioWavelength = true; # Audio visualizer (cava)
     enableCalendarEvents = true; # Calendar integration (khal)
-    enableSystemSound = true; # System sound effects
+    # enableSystemSound = true; # System sound effects
     niri = {
       enableKeybinds = true; # Automatic keybinding configuration
       enableSpawn = true; # Auto-start DMS with niri

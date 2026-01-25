3bc7c741ea0658f2ad077a480c44fb4ad0a68579
Shao-yan (Matt) Ji <github@mail.shaoyanji@is-a.dev>
2026-01-25 08:17:14 +0100

    changed the package option entirely?

--- hosts/poseidon/nvidia.nix
+++ hosts/poseidon/nvidia.nix
@@ -9,7 +9,7 @@
   services.xserver.videoDrivers = ["amdgpu" "nvidia"];
   hardware.nvidia = {
     open = lib.mkForce true;
-    # package = config.boot.kernelPackages.nvidiaPackages.beta;
+    package = "beta";
     prime = {
       nvidiaBusId = "PCI:1:0:0";
       amdgpuBusId = "PCI:5:0:0";

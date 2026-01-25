e495ae197e7bf0bca8146ca7ef86dbea108ed4f1
Shao-yan (Matt) Ji <github@mail.shaoyanji@is-a.dev>
2026-01-25 08:06:57 +0100

    added log2

--- modules/shell/bash.nix
+++ modules/shell/bash.nix
@@ -22,6 +22,7 @@
         0url() { curl -F"url=$1" https://envs.sh ; }
         0short() { curl -F"shorten=$1" https://envs.sh ; }
         tsup() { curl -F "file=@$1" https://temp.sh/upload; }
+        log2() { local n=0; for ((i=$1-1; i>0; i>>=1)); do ((n+=1)); done; echo $n; }
       '';
   };
   home.sessionVariables = {

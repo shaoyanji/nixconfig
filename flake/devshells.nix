29742f74caaac6b686501a2c330b0f1ecff8b44c
matt <github@mail.shaoyanji@is-a.dev>
2026-03-29 21:13:48 +0000

    reverted out of dev container

--- flake/devshells.nix
+++ flake/devshells.nix
@@ -9,21 +9,9 @@
     in {
       default = pkgs.mkShell {
         nativeBuildInputs = [
-          pkgs.age
-          pkgs.bat
-          pkgs.direnv
-          pkgs.eza
-          pkgs.fd
-          pkgs.git
           pkgs.nodejs
           pkgs.go
           pkgs.gopls
-          pkgs.go-task
-          pkgs.gum
-          pkgs.jq
-          pkgs.ripgrep
-          pkgs.sops
-          pkgs.yq-go
         ];
       };
     }

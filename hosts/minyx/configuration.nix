78c2c5bf05ec95e4baefc7b207d7c94b0bb2acf0
Shao-yan (Matt) Ji <github@mail.shaoyanji@is-a.dev>
2026-04-09 21:16:47 +0200

    chore: remove nix-refactor agent skill and update minyx boot
    Removes the internal documentation for the `nix-refactor` agent skill.
    Explicitly disables `systemd-boot` for the `minyx` host configuration.

--- hosts/minyx/configuration.nix
+++ hosts/minyx/configuration.nix
@@ -4,6 +4,7 @@
   ];
 
   boot.loader.grub.enable = false;
+  boot.loader.systemd-boot.enable = lib.mkForce false;
   boot.loader.generic-extlinux-compatible.enable = true;
 
   system.stateVersion = "24.11";

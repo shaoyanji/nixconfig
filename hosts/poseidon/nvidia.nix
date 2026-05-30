e626601d04a48a9f6f3a191ee310e0287085d6ff
Shao-yan (Matt) Ji <github@mail.shaoyanji@is-a.dev>
2026-05-30 20:00:04 +0200

    nvidia

--- hosts/poseidon/nvidia.nix
+++ hosts/poseidon/nvidia.nix
@@ -8,16 +8,13 @@
   ];
   services.xserver.videoDrivers = ["amdgpu" "nvidia"];
   hardware.nvidia = {
-    open = lib.mkForce true;
+    open = false;
     package = config.boot.kernelPackages.nvidiaPackages.stable;
-    nvidiaPersistenced = lib.mkForce false;
+    powerManagement.enable = lib.mkForce true;
     prime = {
       nvidiaBusId = "PCI:1:0:0";
       amdgpuBusId = "PCI:5:0:0";
     };
   };
 
-  # Disable NVIDIA GSP firmware via kernel parameter to fix boot failure
-  # The kernel_gsp.c assertion failure is caused by GSP firmware issues
-  boot.kernelParams = ["nvidia.NVreg_EnableGpuFirmware=0"];
 }

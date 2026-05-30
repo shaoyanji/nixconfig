1b86460a161b0e43595f463468c9e07855f0c923
matt <github@mail.shaoyanji.is-a.dev>
2026-05-30 02:43:20 +0000

    fix: correct niri-flake HM module path (homeModules.niri, not homeManagerModules.default)

--- modules/global/home-manager-shared.nix
+++ modules/global/home-manager-shared.nix
@@ -8,7 +8,7 @@
     inputs.sops-nix.homeManagerModules.sops
     inputs.kickstart-nixvim.homeManagerModules.default
     inputs.nix-index-database.homeModules.nix-index
-    inputs.niri.homeManagerModules.default
+    inputs.niri.homeModules.niri
     inputs.dms.homeModules.dank-material-shell
     inputs.dms.homeModules.niri
   ];
